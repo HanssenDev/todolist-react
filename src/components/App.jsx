6845eff44f13639c4edd1d9ece193e244b7f57d6
Hanssen <hramanjooloo@gmail.com>
2020-08-18 20:33:10 +0100

    create Input and ToDoItem components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,11 @@ import React, { useState } from "react";
 
 function App() {
     return (
-        
+        <div className="container">
+            <div className="heading">
+                <h1>To-Do List</h1>
+            </div>
+        </div>
     )
 }
 
