cfa7d16860892a9af4da54a561c2ec2a4a5e8eae
Hanssen <hramanjooloo@gmail.com>
2020-08-18 20:38:08 +0100

    add logic for app

--- src/components/InputArea.jsx
+++ src/components/InputArea.jsx
@@ -1,13 +1,35 @@
 import React, { useState } from "react";
 
-function InputArea() {
-    return (
-        <div className="form">
-            
-        </div>
-        
-    )
-}
+function InputArea(props) {
+  const [inputText, setInputText] = useState("");
 
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
+
+  const newItem = () => {
+    props.onClick(inputText);
+    setInputText("");
+  };
+
+  function handleKeyPress(e) {
+    e.which === 13 && newItem();
+  }
+
+  return (
+    <div className="form">
+      <input
+        onChange={handleChange}
+        onKeyPress={handleKeyPress}
+        type="text"
+        value={inputText}
+      />
+      <button onClick={newItem}>
+        <span>Add</span>
+      </button>
+    </div>
+  );
+}
 
 export default InputArea;
--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
-function ToDoItem() {
-    return (
-        <div>
-            
-        </div>
-    )
+function ToDoItem(props) {
+  return (
+    <div
+      onClick={() => {
+        props.onChecked(props.id);
+      }}
+    >
+      <li>{props.text}</li>
+    </div>
+  );
 }
-
 
 export default ToDoItem;
